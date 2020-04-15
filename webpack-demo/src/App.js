8f36d76933f9eff8b0ae9300dbdba3f3cce5be63
edircabsan <edircabsan@gmail.com>
2020-04-15 20:36:02 -0300

    Commit after module 22 - Working with webpack

---
+++ webpack-demo/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Route } from 'react-router';
+
+import Users from './containers/Users';
+
+import asyncComponent from './hoc/asyncComponent';
+
+const AsyncPizza = asyncComponent(() => {
+    return import('./containers/Pizza');
+})
+
+class App extends Component {
+    render () {
+        return (
+            <div>
+                <div>
+                    <Link to="/">Users</Link>
+                    <Link to="/pizza">Pizza</Link>
+                </div>
+                <div>
+                    <Route path="/" exact component={Users} />
+                    <Route path="/pizza" exact component={AsyncPizza} />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
